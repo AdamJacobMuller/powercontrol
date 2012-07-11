6911fc3777819bd3c1e8357a77984a22affea9dd
Adam Jacob Muller <adam@isprime.com>
2012-07-10 20:47:50 -0400

    fix TEMPLATE_DIRS

--- power/settings.py
+++ power/settings.py
@@ -100,7 +100,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/adam/Scripts/sites/power.adam.gs/templates'
+    'templates'
 )
 
 INSTALLED_APPS = (
